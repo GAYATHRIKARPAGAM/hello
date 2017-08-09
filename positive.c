35b641d2140b2fc84d24037c6257e83f6d0956d4
GAYATHRIKARPAGAM <30749321+GAYATHRIKARPAGAM@users.noreply.github.com>
2017-08-09 15:54:45 +0530

    positive.c

---
+++ positive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int num;
+scanf("%d",&num);
+if(num==0)
+printf("the number is zero");
+else if(num>0)
+printf("the number is positive");
+else
+printf("the number is negative");
+return 0;
+}
